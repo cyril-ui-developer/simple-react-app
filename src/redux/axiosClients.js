18d0aebc6d062e2bdcad77a5e7199baf395b783d
Cyril Ajieh <cyril.ajieh@ibm.com>
2019-04-13 10:35:46 -0400

    Updated endpoint

--- src/redux/axiosClients.js
+++ src/redux/axiosClients.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 
-// const reqResAPI = 'https://reqres.in/api/';
-const mockServer = 'http://localhost:3000/'; // use mock server for better simulation of GET, POST and PUT requests
-const JSON_BASE_URL = mockServer;
+const reqResAPI = 'https://reqres.in/api/';
+// const mockServer = 'http://localhost:3000/'; // use mock server for better simulation of GET, POST and PUT requests
+const JSON_BASE_URL = reqResAPI;
 
 const defaultClient = axios.create({
   baseURL: JSON_BASE_URL,
