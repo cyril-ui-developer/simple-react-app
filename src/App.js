5b5901623503fd9dae3fd04e964ededfdb0ab553
Cyril Ajieh <cyril.ajieh@ibm.com>
2019-04-16 11:44:12 -0400

    Implemented router switch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import {
-  Route,
+  Route, Redirect, Switch,
 } from 'react-router';
 
 import UsersDashboard from './containers/usersdashboard/usersDashboard';
+import PageNotFound from './components/pagenotfound/pageNotFound';
 import './app.css';
 
 class App extends Component {
@@ -12,10 +13,13 @@ class App extends Component {
       <main>
         <header className="header-primary"><h2>Simple React App</h2></header>
         <section>
+
           <div style={{ width: 1000, margin: '0 auto' }}>
-            <Route exact path="/" component={UsersDashboard} />
-            <Route path="/users" component={UsersDashboard} />
-            {/* <Redirect from="/" to="/users" />  */}
+            <Switch>
+              <Route exact path="/" component={UsersDashboard} />
+              <Redirect from="/users" to="/" />
+              <Route component={PageNotFound} />
+            </Switch>
           </div>
         </section>
         <header className="footer-primary">...</header>
